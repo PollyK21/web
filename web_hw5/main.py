c9b23974cf73470101b1af5aa3ee90e852b9d66c
PollyK21 <pol.kulish@gmail.com>
2024-02-29 01:03:31 +0200

    new

--- web_hw5/main.py
+++ web_hw5/main.py
@@ -1,11 +1,7 @@
 import aiohttp
 import asyncio
 from datetime import datetime, timedelta
-import sys
 import json
-
-# python3 main.py 2
-# python3 main.py 2 PLN
 
 
 class CurrencyAPI:
@@ -20,7 +16,7 @@ class CurrencyAPI:
                 else:
                     raise Exception(f"Failed to fetch data: {response.status}")
 
-    async def get_currency_rates(self, days):
+    async def get_currency_rates(self, days, curency_list):
         current_date = datetime.now()
         all_rates = {}
         # цикл по колву дней заданому от сегодня
@@ -31,7 +27,8 @@ class CurrencyAPI:
                 exchange_rates = data.get('exchangeRate', [])
                 rates = {}
                 for rate in exchange_rates:
-                    rates[rate["currency"]] = {"sale": rate["saleRateNB"], "purchase": rate["purchaseRateNB"]}
+                    if rate["currency"] in ['USD', 'EUR'] or rate["currency"] in curency_list:
+                        rates[rate["currency"]] = {"sale": rate["saleRateNB"], "purchase": rate["purchaseRateNB"]}
             except Exception as e:
                 print(f"Error fetching data for {date.strftime('%d.%m.%Y')}: {e}")
             all_rates[date.strftime("%d.%m.%Y")] = rates
@@ -44,29 +41,29 @@ class CurrencyAPI:
 
 
 async def main():
-    if len(sys.argv) not in [2,3]:
-        print("Вкажіть кількість днів.")
-    else:
-        days = int(sys.argv[1])
-        if len(sys.argv) > 2:
-            currency = sys.argv[2]
-            currency.upper()
+    filename = 'data.json'
+    try:
+        answer = int(input("Вкажіть кількість днів для статистики (не більше 10) - "))
+        another = input("Бажаєте додати валюти до переліку?(USD та EUR за замовченням) Y/N - ").upper()
+        if another == "Y":
+            currency = input("Вкажіть додаткові валюти через пробіл - ").upper()
+            curency_list = currency.split()
         else:
-            currency = None
-        filename = 'data.json'
-        if days <= 10:
+            curency_list = []
+        if 1 <= answer <= 10:
             api = CurrencyAPI()
             try:
-                currency_rates = await api.get_currency_rates(days)
+                currency_rates = await api.get_currency_rates(answer, curency_list)
                 await api.write_to_json_file(currency_rates, filename)
                 for date, rates in currency_rates.items():
                     for rate, data in rates.items():
-                        if rate in ['USD', 'EUR'] or rate == currency:
-                            print(f"{date}, {rate}: sale = {data['sale']}, purchase = {data['purchase']},")
+                        print(f"{date}, {rate}: sale = {data['sale']}, purchase = {data['purchase']},")
             except Exception as e:
                 print(f"An error occurred: {e}")
         else:
-            print("Максимальна кількість днів - 10.")
+            print("Кількість днів має бути в діапазоні від 1 до 10")
+    except ValueError:
+        print("Ошибка: введите целое число")
 
 if __name__ == "__main__":
     asyncio.run(main())
